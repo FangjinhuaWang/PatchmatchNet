c16743866eee53289669d278fc4bb9a2669983ee
Antonios Matakos <anmatako@microsoft.com>
2021-09-15 13:52:11 -0700

    Unify MVSDataset and scripts for evaluation

--- eval.sh
+++ eval.sh
@@ -1,41 +1,26 @@
 #!/usr/bin/env bash
 
+CHECKPOINT_FILE="./checkpoints/model_000007.ckpt"
+
 # test on DTU's evaluation set
 DTU_TESTING="/home/dtu/"
-CKPT_FILE="./checkpoints/model_000007.ckpt"
-python eval.py --dataset=dtu_yao_eval --batch_size=1 --n_views 5 \
---patchmatch_iteration 1 2 2 --patchmatch_range 6 4 2 \
---patchmatch_num_sample 8 8 16 --propagate_neighbors 0 8 16 --evaluate_neighbors 9 9 9 \
---patchmatch_interval_scale 0.005 0.0125 0.025 \
---testpath=$DTU_TESTING --geo_pixel_thres=1 --geo_depth_thres=0.01 --photo_thres 0.8 \
---outdir=./outputs --testlist lists/dtu/test.txt --loadckpt $CKPT_FILE $@
+python eval.py --scan_list lists/dtu/test.txt --input_folder=$DTU_TESTING --output_folder=$DTU_TESTING \
+--checkpoint_path $CHECKPOINT_FILE --parallel --num_views 5 --image_max_dim 1600 --geo_mask_thres 3 --photo_thres 0.8 "$@"
 
 # -------------------------------------------------------------------------------------
 # test on eth3d benchmark
-ETH3d_TESTING="/home/eth3d_high_res_test/"
-# python eval_eth.py --dataset=eth3d --split train --batch_size=1 --n_views 7 \
-# --patchmatch_iteration 1 2 2 --patchmatch_range 6 4 2 \
-# --patchmatch_num_sample 8 8 16 --propagate_neighbors 0 8 16 --evaluate_neighbors 9 9 9 \
-# --patchmatch_interval_scale 0.005 0.0125 0.025 \
-# --testpath=$ETH3d_TESTING --geo_pixel_thres=1 --geo_depth_thres=0.01 --photo_thres=0.6 \
-# --outdir ./outputs_eth --loadckpt $CKPT_FILE $@
+#ETH3D_TESTING="/home/eth3d_high_res_test/"
+#python eval.py --scan_list lists/eth3d/test.txt --input_folder=$ETH3D_TESTING --output_folder=$ETH3D_TESTING \
+#--checkpoint_path $CHECKPOINT_FILE --parallel --num_views 7 --image_max_dim 2688 --geo_mask_thres 2 --photo_thres 0.6 "$@"
 
 # -------------------------------------------------------------------------------------
 # test on tanks & temples
-TANK_TESTING="/home/TankandTemples/"
-# python eval_tank.py --dataset=tanks --split intermediate --batch_size=1 --n_views 7 \
-# --patchmatch_iteration 1 2 2 --patchmatch_range 6 4 2 \
-# --patchmatch_num_sample 8 8 16 --propagate_neighbors 0 8 16 --evaluate_neighbors 9 9 9 \
-# --patchmatch_interval_scale 0.005 0.0125 0.025 \
-# --testpath=$TANK_TESTING --geo_pixel_thres=1 --geo_depth_thres=0.01 \
-# --outdir ./outputs_tanks --loadckpt $CKPT_FILE $@
+#TANK_TESTING="/home/TankandTemples/"
+#python eval.py --scan_list lists/tanks/intermediate.txt --input_folder=$TANK_TESTING --output_folder=$TANK_TESTING \
+#--checkpoint_path $CHECKPOINT_FILE --parallel --num_views 7 --image_max_dim 2048 --geo_mask_thres 5 --photo_thres 0.8 "$@"
 
 # -------------------------------------------------------------------------------------
 # test on your custom dataset
-CUSTOM_TESTING="/home/custom/"
-# python eval_custom.py --dataset=custom --batch_size=1 --n_views 5 \
-# --patchmatch_iteration 1 2 2 --patchmatch_range 6 4 2 \
-# --patchmatch_num_sample 8 8 16 --propagate_neighbors 0 8 16 --evaluate_neighbors 9 9 9 \
-# --patchmatch_interval_scale 0.005 0.0125 0.025 \
-# --testpath=$CUSTOM_TESTING --geo_pixel_thres=1 --geo_depth_thres=0.01 --photo_thres 0.8 \
-# --outdir ./outputs_custom --loadckpt $CKPT_FILE $@
+#CUSTOM_TESTING="/home/custom/"
+#python eval.py --input_folder=$CUSTOM_TESTING --output_folder=$CUSTOM_TESTING --checkpoint_path $CHECKPOINT_FILE \
+#--parallel --num_views 10 --image_max_dim 2048 --geo_mask_thres 5 --photo_thres 0.5 "$@"
