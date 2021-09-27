209819d1bb1455f3a0b5debe569ca5c445e3ff48
Antonios Matakos <anmatako@microsoft.com>
2021-09-27 10:04:56 -0700

    Consolidate read/write operations in data_io

--- datasets/data_io.py
+++ datasets/data_io.py
@@ -1,10 +1,226 @@
-"""Utilities for reading and writing depth maps from/to disk."""
+"""Utilities for reading and writing images, depth maps, and auxiliary data (cams, pairs) from/to disk."""
 
 import re
+import struct
 import sys
-from typing import Tuple
+from typing import Dict, List, Tuple
 
+import cv2
 import numpy as np
+from PIL import Image
+
+
+def scale_to_max_dim(image: np.ndarray, max_dim: int) -> Tuple[np.ndarray, int, int]:
+    """Scale image to specified max dimension
+
+    Args:
+        image: the input image in original size
+        max_dim: the max dimension to scale the image down to if smaller than the actual max dimension
+
+    Returns:
+        Tuple of scaled image along with original image height and width
+    """
+    original_height = image.shape[0]
+    original_width = image.shape[1]
+    scale = max_dim / max(original_height, original_width)
+    if 0 < scale < 1:
+        width = int(scale * original_width)
+        height = int(scale * original_height)
+        image = cv2.resize(image, (width, height), interpolation=cv2.INTER_LINEAR)
+
+    return image, original_height, original_width
+
+
+def read_image(filename: str, max_dim: int = -1) -> Tuple[np.ndarray, int, int]:
+    """Read image and rescale to specified max dimension (if exists)
+
+    Args:
+        filename: image input file path string
+        max_dim: max dimension to scale down the image; keep original size if -1
+
+    Returns:
+        Tuple of scaled image along with original image height and width
+    """
+    image = Image.open(filename)
+    # scale 0~255 to 0~1
+    np_image = np.array(image, dtype=np.float32) / 255.0
+    return scale_to_max_dim(np_image, max_dim)
+
+
+def save_image(filename: str, image: np.ndarray) -> None:
+    """Save images including binary mask (bool), float (0<= val <= 1), or int (as-is)
+
+    Args:
+        filename: image output file path string
+        image: output image array
+    """
+    if image.dtype == bool:
+        image = image.astype(np.uint8) * 255
+    elif image.dtype == np.float32 or image.dtype == np.float64:
+        image = image * 255
+        image = image.astype(np.uint8)
+    else:
+        image = image.astype(np.uint8)
+    Image.fromarray(image).save(filename)
+
+
+def read_image_dictionary(filename: str) -> Dict[int, str]:
+    """Create image dictionary from file; useful for ETH3D dataset reading and conversion.
+
+    Args:
+        filename: input dictionary text file path
+
+    Returns:
+        Dictionary of image id (int) and corresponding image file name (string)
+    """
+    image_dict: Dict[int, str] = {}
+    with open(filename) as f:
+        num_entries = int(f.readline().strip())
+        for _ in range(num_entries):
+            parts = f.readline().strip().split(' ')
+            image_dict[int(parts[0].strip())] = parts[1].strip()
+    return image_dict
+
+
+def read_cam_file(filename: str) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """Read camera intrinsics, extrinsics, and depth values (min, max) from text file
+
+    Args:
+        filename: cam text file path string
+
+    Returns:
+        Tuple with intrinsics matrix (3x3), extrinsics matrix (4x4), and depth params vector (min and max) if exists
+    """
+    with open(filename) as f:
+        lines = [line.rstrip() for line in f.readlines()]
+    # extrinsics: line [1,5), 4x4 matrix
+    extrinsics = np.fromstring(' '.join(lines[1:5]), dtype=np.float32, sep=' ').reshape((4, 4))
+    # intrinsics: line [7-10), 3x3 matrix
+    intrinsics = np.fromstring(' '.join(lines[7:10]), dtype=np.float32, sep=' ').reshape((3, 3))
+    # depth min and max: line 11
+    if len(lines) >= 12:
+        depth_params = np.fromstring(lines[11], dtype=np.float32, sep=' ')
+    else:
+        depth_params = np.empty(0)
+
+    return intrinsics, extrinsics, depth_params
+
+
+def read_pair_file(filename: str) -> List[Tuple[int, List[int]]]:
+    """Read image pairs from text file and output a list of tuples each containing the reference image ID and a list of
+    source image IDs
+
+    Args:
+        filename: pair text file path string
+
+    Returns:
+        List of tuples with reference ID and list of source IDs
+    """
+    data = []
+    with open(filename) as f:
+        num_viewpoint = int(f.readline())
+        for _ in range(num_viewpoint):
+            ref_view = int(f.readline().rstrip())
+            src_views = [int(x) for x in f.readline().rstrip().split()[1::2]]
+            if len(src_views) != 0:
+                data.append((ref_view, src_views))
+    return data
+
+
+def read_map(path: str, max_dim: int = -1) -> np.ndarray:
+    """ Read a binary depth map from either PFM or Colmap (bin) format determined by the file extension and also scale
+    the map to the max dim if given
+
+    Args:
+        path: input depth map file path string
+        max_dim: max dimension to scale down the map; keep original size if -1
+
+    Returns:
+        Array of depth map values
+    """
+    if path.endswith('.bin'):
+        in_map = read_bin(path)
+    elif path.endswith('.pfm'):
+        in_map, _ = read_pfm(path)
+    else:
+        raise Exception('Invalid input format; only pfm and bin are supported')
+    return scale_to_max_dim(in_map, max_dim)[0]
+
+
+def save_map(path: str, data: np.ndarray) -> None:
+    """Save binary depth or confidence maps in PFM or Colmap (bin) format determined by the file extension
+
+    Args:
+        path: output map file path string
+        data: map data array
+    """
+    if path.endswith('.bin'):
+        save_bin(path, data)
+    elif path.endswith('.pfm'):
+        save_pfm(path, data)
+    else:
+        raise Exception('Invalid input format; only pfm and bin are supported')
+
+
+def read_bin(path: str) -> np.ndarray:
+    """Read a depth map from a Colmap .bin file
+
+    Args:
+        path: .pfm file path string
+
+    Returns:
+        data: array of shape (H, W, C) representing loaded depth map
+    """
+    with open(path, 'rb') as fid:
+        width, height, channels = np.genfromtxt(fid, delimiter='&', max_rows=1,
+                                                usecols=(0, 1, 2), dtype=int)
+        fid.seek(0)
+        num_delimiter = 0
+        byte = fid.read(1)
+        while True:
+            if byte == b'&':
+                num_delimiter += 1
+                if num_delimiter >= 3:
+                    break
+            byte = fid.read(1)
+        data = np.fromfile(fid, np.float32)
+    data = data.reshape((width, height, channels), order='F')
+    data = np.transpose(data, (1, 0, 2))
+    return data
+
+
+def save_bin(filename: str, data: np.ndarray):
+    """Save a depth map to a Colmap .bin file
+
+    Args:
+        filename: output .pfm file path string,
+        data: depth map to save, of shape (H,W) or (H,W,C)
+    """
+    if data.dtype != np.float32:
+        raise Exception('Image data type must be float32.')
+
+    if len(data.shape) == 2:
+        height, width = data.shape
+        channels = 1
+    elif len(data.shape) == 3 and (data.shape[2] == 3 or data.shape[2] == 1):
+        height, width, channels = data.shape
+    else:
+        raise Exception('Image must have H x W x 3, H x W x 1 or H x W dimensions.')
+
+    with open(filename, 'w') as fid:
+        fid.write(str(width) + '&' + str(height) + '&' + str(channels) + '&')
+
+    with open(filename, 'ab') as fid:
+        if len(data.shape) == 2:
+            image_trans = np.transpose(data, (1, 0))
+        else:
+            image_trans = np.transpose(data, (1, 0, 2))
+        data_1d = image_trans.reshape(-1, order='F')
+        data_list = data_1d.tolist()
+        endian_character = '<'
+        format_char_sequence = ''.join(['f'] * len(data_list))
+        byte_data = struct.pack(endian_character + format_char_sequence, *data_list)
+        fid.write(byte_data)
 
 
 def read_pfm(filename: str) -> Tuple[np.ndarray, float]:
@@ -17,12 +233,7 @@ def read_pfm(filename: str) -> Tuple[np.ndarray, float]:
         data: array of shape (H, W, C) representing loaded depth map
         scale: float to recover actual depth map pixel values
     """
-    file = open(filename, "rb") # treat as binary and read-only
-    color = None
-    width = None
-    height = None
-    scale = None
-    endian = None
+    file = open(filename, "rb")  # treat as binary and read-only
 
     header = file.readline().decode("utf-8").rstrip()
     if header == "PF":
@@ -55,7 +266,7 @@ def read_pfm(filename: str) -> Tuple[np.ndarray, float]:
 
 
 def save_pfm(filename: str, image: np.ndarray, scale: float = 1) -> None:
-    """Save a depth map from a .pfm file
+    """Save a depth map to a .pfm file
 
     Args:
         filename: output .pfm file path string,
--- datasets/dtu_yao_eval.py
+++ datasets/dtu_yao_eval.py
@@ -1,80 +1,34 @@
 from torch.utils.data import Dataset
+from datasets.data_io import read_cam_file, read_pair_file, read_image
+from typing import List, Tuple
 import numpy as np
 import os
-from PIL import Image
-from datasets.data_io import *
 import cv2
 
 
 class MVSDataset(Dataset):
-    def __init__(self, datapath, listfile, mode, nviews, img_wh=(1600, 1200), **kwargs):
+    def __init__(self, datapath, listfile, mode, nviews, img_wh=(1600, 1200)):
         super(MVSDataset, self).__init__()
         
         self.stages = 4
         self.datapath = datapath
-        self.listfile = listfile
-        self.mode = mode
         self.nviews = nviews
         self.img_wh = img_wh
-        
 
-        assert self.mode == "test"
-        self.metas = self.build_list()
+        assert mode == "test"
 
-    def build_list(self):
-        metas = []
-        with open(self.listfile) as f:
+        with open(listfile) as f:
             scans = f.readlines()
             scans = [line.rstrip() for line in scans]
-            
-                  
+
+        self.metas: List[Tuple[str, int, List[int]]] = []
         for scan in scans:
-            pair_file = "{}/pair.txt".format(scan)
-            # read the pair file
-            with open(os.path.join(self.datapath, pair_file)) as f:
-                num_viewpoint = int(f.readline())
-                # viewpoints (49)
-                for view_idx in range(num_viewpoint):
-                    ref_view = int(f.readline().rstrip())
-                    src_views = [int(x) for x in f.readline().rstrip().split()[1::2]]
-                    metas.append((scan, ref_view, src_views))
-        print("dataset", self.mode, "metas:", len(metas))
-        return metas
+            pair_data = read_pair_file(os.path.join(self.datapath, scan, 'pair.txt'))
+            self.metas += [(scan, ref, src) for ref, src in pair_data]
+        print("dataset", mode, "metas:", len(self.metas))
 
     def __len__(self):
         return len(self.metas)
-
-    def read_cam_file(self, filename):
-        with open(filename) as f:
-            lines = f.readlines()
-            lines = [line.rstrip() for line in lines]
-        # extrinsics: line [1,5), 4x4 matrix
-        extrinsics = np.fromstring(' '.join(lines[1:5]), dtype=np.float32, sep=' ').reshape((4, 4))
-        # intrinsics: line [7-10), 3x3 matrix
-        intrinsics = np.fromstring(' '.join(lines[7:10]), dtype=np.float32, sep=' ').reshape((3, 3))
-
-        depth_min = float(lines[11].split()[0])
-        depth_max = float(lines[11].split()[1])
-        return intrinsics, extrinsics, depth_min, depth_max
-
-    
-    def read_img(self, filename):
-        img = Image.open(filename)
-        # scale 0~255 to 0~1
-        np_img = np.array(img, dtype=np.float32) / 255.
-        np_img = cv2.resize(np_img, self.img_wh, interpolation=cv2.INTER_LINEAR)
-        
-        h, w, _ = np_img.shape
-        
-        np_img_ms = {
-            "stage_3": cv2.resize(np_img, (w//8, h//8), interpolation=cv2.INTER_LINEAR),
-            "stage_2": cv2.resize(np_img, (w//4, h//4), interpolation=cv2.INTER_LINEAR),
-            "stage_1": cv2.resize(np_img, (w//2, h//2), interpolation=cv2.INTER_LINEAR),
-            "stage_0": np_img
-        }
-        return np_img_ms
-        
-
 
     def __getitem__(self, idx):
         meta = self.metas[idx]
@@ -99,63 +53,57 @@ class MVSDataset(Dataset):
             img_filename = os.path.join(self.datapath, '{}/images/{:0>8}.jpg'.format(scan, vid))
             proj_mat_filename = os.path.join(self.datapath, '{}/cams_1/{:0>8}_cam.txt'.format(scan, vid))
 
-            imgs = self.read_img(img_filename)
-            imgs_0.append(imgs['stage_0'])
-            imgs_1.append(imgs['stage_1'])
-            imgs_2.append(imgs['stage_2'])
-            imgs_3.append(imgs['stage_3'])
+            image, original_h, original_w = read_image(img_filename, max(self.img_wh))
+            imgs_0.append(image)
+            imgs_1.append(cv2.resize(image, (self.img_wh[0]//2, self.img_wh[1]//2), interpolation=cv2.INTER_LINEAR))
+            imgs_2.append(cv2.resize(image, (self.img_wh[0]//4, self.img_wh[1]//4), interpolation=cv2.INTER_LINEAR))
+            imgs_3.append(cv2.resize(image, (self.img_wh[0]//8, self.img_wh[1]//8), interpolation=cv2.INTER_LINEAR))
 
-            intrinsics, extrinsics, depth_min_, depth_max_ = self.read_cam_file(proj_mat_filename)
+            intrinsics, extrinsics, depth_params = read_cam_file(proj_mat_filename)
             intrinsics[0] *= self.img_wh[0]/img_w
             intrinsics[1] *= self.img_wh[1]/img_h
             # multiply intrinsics and extrinsics to get projection matrix
             proj_mat = extrinsics.copy()
-            intrinsics[:2,:] *= 0.125
+            intrinsics[:2, :] *= 0.125
             proj_mat[:3, :4] = np.matmul(intrinsics, proj_mat[:3, :4])
             proj_matrices_3.append(proj_mat)
 
             proj_mat = extrinsics.copy()
-            intrinsics[:2,:] *= 2
+            intrinsics[:2, :] *= 2
             proj_mat[:3, :4] = np.matmul(intrinsics, proj_mat[:3, :4])
             proj_matrices_2.append(proj_mat)
 
             proj_mat = extrinsics.copy()
-            intrinsics[:2,:] *= 2
+            intrinsics[:2, :] *= 2
             proj_mat[:3, :4] = np.matmul(intrinsics, proj_mat[:3, :4])
             proj_matrices_1.append(proj_mat)
 
             proj_mat = extrinsics.copy()
-            intrinsics[:2,:] *= 2
+            intrinsics[:2, :] *= 2
             proj_mat[:3, :4] = np.matmul(intrinsics, proj_mat[:3, :4])
             proj_matrices_0.append(proj_mat)
 
-
             if i == 0:  # reference view
-                depth_min = depth_min_
-                depth_max = depth_max_
+                depth_min = depth_params[0]
+                depth_max = depth_params[1]
 
-        imgs_0 = np.stack(imgs_0).transpose([0, 3, 1, 2])
-        imgs_1 = np.stack(imgs_1).transpose([0, 3, 1, 2])
-        imgs_2 = np.stack(imgs_2).transpose([0, 3, 1, 2])
-        imgs_3 = np.stack(imgs_3).transpose([0, 3, 1, 2])
-        imgs = {}
-        imgs['stage_0'] = imgs_0
-        imgs['stage_1'] = imgs_1
-        imgs['stage_2'] = imgs_2
-        imgs['stage_3'] = imgs_3
+        # imgs: N*3*H0*W0, N is number of images
+        imgs = {
+            'stage_0': np.stack(imgs_0).transpose([0, 3, 1, 2]),
+            'stage_1': np.stack(imgs_1).transpose([0, 3, 1, 2]),
+            'stage_2': np.stack(imgs_2).transpose([0, 3, 1, 2]),
+            'stage_3': np.stack(imgs_3).transpose([0, 3, 1, 2])
+        }
         # proj_matrices: N*4*4
-        proj_matrices_0 = np.stack(proj_matrices_0)
-        proj_matrices_1 = np.stack(proj_matrices_1)
-        proj_matrices_2 = np.stack(proj_matrices_2)
-        proj_matrices_3 = np.stack(proj_matrices_3)
-        proj={}
-        proj['stage_3']=proj_matrices_3
-        proj['stage_2']=proj_matrices_2
-        proj['stage_1']=proj_matrices_1
-        proj['stage_0']=proj_matrices_0
+        proj = {
+            'stage_3': np.stack(proj_matrices_3),
+            'stage_2': np.stack(proj_matrices_2),
+            'stage_1': np.stack(proj_matrices_1),
+            'stage_0': np.stack(proj_matrices_0)
+        }
 
         return {"imgs": imgs,                   # N*3*H0*W0
-                "proj_matrices": proj, # N*4*4
+                "proj_matrices": proj,          # N*4*4
                 "depth_min": depth_min,         # scalar
                 "depth_max": depth_max,         # scalar
                 "filename": scan + '/{}/' + '{:0>8}'.format(view_ids[0]) + "{}"}
